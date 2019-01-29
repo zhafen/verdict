cb6596f27c8a8f06d5b83009aefa8ace928f492d
Zach Hafen <zachary.h.hafen@gmail.com>
2019-01-29 11:07:13 -0600

    Added installation files.

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="verdict",
+    version="1.0",
+    author="Zach Hafen",
+    author_email="zachary.h.hafen@gmail.com",
+    description="A version of Python's dictionary with additional features",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zhafen/verdict",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pandas>=0.20.3',
+        'mock>=2.0.0',
+        'numpy>=1.14.5',
+        'six>=1.10.0',
+        'setuptools>=28.8.0',
+        'h5py>=2.7.0',
+    ],
+)
